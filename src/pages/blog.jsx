608e6497b77f9be55cd0432d0fd9967fc7d1dbd8
samah-abroud <154094128+samah-abroud@users.noreply.github.com>
2025-04-08 16:58:06 +0100

    Update blog.jsx

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function blog() {
-  return <div className='contact'>
-  <h1> Page</h1>
+  return<div className=' ' id='blog'>
+  <div className="flex flex-col justify-center pt-16 items-center  text-blue-600 font-semibold text-s text-center">
+    <p >Blog</p>
+    <span className='text-center' >___</span>
+  </div>
+  <h1 className='text-3xl font-bold text-center mt-6  '>Récent blog</h1>
+  <p className="mt-4 text-center text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br /> Reiciendis, non reprehenderit adipisci repellat corrupti.</p>
+
+
 </div>;  
 }
 
